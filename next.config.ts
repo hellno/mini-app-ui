f214c2c9d901f5157e7de689f5ebf80fe040e31e
nonom <nounsplayground@gmail.com>
2025-05-26 02:18:15 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
+  },
 };
 
 export default nextConfig;
