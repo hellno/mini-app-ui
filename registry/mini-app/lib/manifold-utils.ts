59191dbc7110e7de5551a0a7958ca3d68ccb5bbc
hellno <hellno@users.noreply.github.com>
2025-07-30 18:24:23 +0200

    remove outdated files that are now in NFTMintButton

--- registry/mini-app/lib/manifold-utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated This file is kept for backward compatibility. 
- * Please use nft-metadata-utils.ts instead which provides the same functionality plus enhanced metadata support.
- */
-export * from "@/registry/mini-app/lib/nft-metadata-utils";
-
-// Re-export the deprecated function name for backward compatibility
-export { getTokenMetadataURL as getTokenURIWithManifoldSupport } from "@/registry/mini-app/lib/nft-metadata-utils";
